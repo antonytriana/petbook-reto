ad78e4a4cb95e22d5d474554512a5092d00f946d
Antony Triana <antonytriana@gmail.com>
2021-10-21 13:11:48 -0500

    Pruebas Unitarias ImagenService

--- src/app/services/imagen.service.spec.ts
+++ src/app/services/imagen.service.spec.ts
@@ -10,7 +10,48 @@ describe('ImagenService', () => {
     service = TestBed.inject(ImagenService);
   });
 
-  it('should be created', () => {
+  it('Debe existir ImagenService', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('obtenerImagenes', () => {
+    it('Debe retornar todas las imagenes', () => {
+      const imagenes = service.obtenerImagenes()
+      expect(imagenes.length).toEqual(12);
+    });
+  });
+
+  describe('obtenerCategoria', () => {
+    it('Debe retornar la categoria de perro', () => {
+      const categoria = service.obtenerCategoria('perro');
+      expect(categoria.length).toBeGreaterThan(0);
+    });
+
+    it('Debe retornar la categoria de mascotas', () => {
+      const categoria = service.obtenerCategoria('mascotas');
+      expect(categoria.length).toBeGreaterThan(0);
+    });
+
+    it('Debe retornar indefinido si se busca una categoria que NO existe', () => {
+      const categoria = service.obtenerCategoria('pez');
+      expect(categoria.length).toEqual(0);
+    });
+  });
+
+  describe('cargarImagenesEnGaleria', () => {
+    it('Debe cargar las imagenes en la galeria', () => {
+      const imagenes = service.obtenerImagenes();
+      const galleryRef = service.cargarImagenesEnGaleria();
+      galleryRef.state.subscribe(result => { 
+        expect(result.items?.length).toBeGreaterThan(0);
+      })
+    });
+
+    it('Debe retornar items vacios si las imagenes no se cargan en la galeria', () => {
+      const galleryRef = service.cargarImagenesEnGaleria();
+      galleryRef.state.subscribe(result => { 
+        expect(result.items?.length).toEqual(0);
+      })
+    });
   });
 });
