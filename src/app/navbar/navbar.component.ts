d16f1bf6e7cd7c894177b10b77071ac84e3215e1
Antony Triana <antonytriana@gmail.com>
2021-10-21 11:41:01 -0500

    Comunicacion entre componentes para el tipo de categoria

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output, EventEmitter  } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  @Output() tipoCategoria = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  prueba(){console.log('asfas')}
+  enviarCategoria(categoria: string) {
+    this.tipoCategoria.emit(categoria)
+  }
 }
