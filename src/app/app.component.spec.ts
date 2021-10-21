d60216d9302fc1297d60d56df16bc195ca0052da
Antony Triana <antonytriana@gmail.com>
2021-10-21 13:11:13 -0500

    Pruebas Unitarias AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,22 +14,15 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('Debe existir el AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'petbook-reto'`, () => {
+  it(`Debe tener como título 'petbook-reto'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('petbook-reto');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('petbook-reto app is running!');
-  });
+  }); 
 });
