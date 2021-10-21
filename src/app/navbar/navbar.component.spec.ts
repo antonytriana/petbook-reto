32a2ca0a428991bac1ec0efa1e6f88afcab0b321
Antony Triana <antonytriana@gmail.com>
2021-10-21 13:11:55 -0500

    Pruebas Unitarias NavbarComponent

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
@@ -19,7 +19,17 @@ describe('NavbarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Debe existir NavbarComponent', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Debe emitirse el evento de perro hacer clic', () => {
+    spyOn(component.tipoCategoria, 'emit');
+ 
+    const elementoMascota = fixture.debugElement.query(By.css('#perro'));
+    elementoMascota.triggerEventHandler('click', {});
+ 
+    fixture.detectChanges();
+    expect(component.tipoCategoria.emit).toHaveBeenCalledWith('perro');
+ });
 });
